fb8d62672053bb8a20f7a3b3ac6e525ab9fe5b26
Amber-DiNenna <amberdinenna@gmail.com>
2022-11-21 20:35:39 -0800

    Render test template.

--- kitchen_project/users/views.py
+++ kitchen_project/users/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('hello world!')
+    context = {
+        'message': 'Hello World!'
+    }
+    return render(request, 'users/home.html', context)
